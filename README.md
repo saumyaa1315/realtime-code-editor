3ab3d45ef6e42853ade5329328562e0e9e1ce243
Saumya Singh <saumyasingh976@gmail.com>
2025-07-21 14:26:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# 🧑‍💻 Real-time Code Editor
+
+A collaborative real-time code editor built with the **MERN stack** and **Socket.IO**. This application allows multiple users to edit code simultaneously in real-time, making it ideal for pair programming, live interviews, and remote development.
+
+---
+
+## 🚀 Features
+
+- ⚡ Real-time code synchronization using **Socket.IO**
+- 🧑‍🤝‍🧑 Multi-user live collaboration
+- 🌐 Built with **MERN Stack** (MongoDB, Express, React, Node.js)
+- 🖥️ Simple and clean frontend code editor UI
+- 🔧 Lightweight and easy to set up
+
+---
+
+## 🛠️ Tech Stack
+
+- **Frontend**: React, Vite, Zustand, Tailwind CSS, Socket.IO Client
+- **Backend**: Node.js, Express, Socket.IO
+- **Optional**: MongoDB (if you plan to store sessions or users)
+
+---
+
+### frontend setup
+- cd frontend
+- npm install
+- cd ..
+- npm run dev
+- **OR**
+- npm run build
+- npm run dev
+
+
+### See it live
+- https://real-time-code-editor-frontend-hf9j.onrender.com/
+
+
---
+++ frontend/src/App.jsx
@@ -0,0 +1,198 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import io from "socket.io-client";
+import Editor from "@monaco-editor/react";
+import { v4 as uuidv4 } from "uuid";
+
+const socket = io("https://real-time-code-editor-backend-9ibi.onrender.com");
+
+const App = () => {
+  const [joined, setJoined] = useState(false);
+  const [roomId, setRoomId] = useState("");
+  const [userName, setUserName] = useState("");
+  const [language, setLanguage] = useState("javascript");
+  const [code, setCode] = useState("// Start code here");
+  const [copySuccess, setCopySuccess] = useState("");
+  const [users, setUsers] = useState([]);
+  const [typing, setTyping] = useState("");
+
+  useEffect(() => {
+    socket.on("userJoined", (users) => {
+      setUsers(users);
+    });
+
+    socket.on("codeUpdate", (newCode) => {
+      setCode(newCode);
+    });
+
+    socket.on("userTyping", (user) => {
+      setTyping(`${user.slice(0, 8)}... is typing`);
+      setTimeout(() => setTyping(""), 2000);
+    });
+
+    socket.on("languageUpdate", (newLanguage) => {
+      setLanguage(newLanguage);
+      setCode("");
+    });
+
+    return () => {
+      socket.off("userJoined");
+      socket.off("codeUpdate");
+      socket.off("userTyping");
+      socket.off("languageUpdate");
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleBeforeUnload = () => {
+      socket.emit("leaveRoom");
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
+
+  const joinRoom = () => {
+    console.log(roomId, userName);
+    if (roomId && userName) {
+      socket.emit("join", { roomId, userName });
+      setJoined(true);
+    }
+  };
+
+  const leaveRoom = () => {
+    socket.emit("leaveRoom");
+    setJoined(false);
+    setRoomId("");
+    setUserName("");
+    setCode("// Start code here");
+    setLanguage("javascript");
+  };
+
+  const copyRoomId = () => {
+    navigator.clipboard.writeText(roomId);
+    setCopySuccess("Copied!");
+    setTimeout(() => setCopySuccess(""), 2000);
+  };
+
+  const handleCodeChange = (newCode) => {
+    setCode(newCode);
+    socket.emit("codeChange", { roomId, code: newCode });
+    socket.emit("typing", { roomId, userName });
+  };
+
+  const handleLanguageChange = (e) => {
+    const newLanguage = e.target.value;
+    setLanguage(newLanguage);
+    socket.emit("languageChange", { roomId, language: newLanguage });
+  };
+
+  const generateRoomId = () => {
+    const newRoomId = uuidv4();
+    setRoomId(newRoomId);
+  };
+
+  if (!joined) {
+    return (
+      // <div className="join-container">
+      //   <div className="join-form">
+      //     <h1>Join Code Editor</h1>
+      //     <input
+      //       type="text"
+      //       placeholder="ROOM ID"
+      //       value={roomId}
+      //       onChange={(e) => setRoomId(e.target.value)}
+      //     />
+
+      //     <input
+      //       type="text"
+      //       placeholder="Your Name"
+      //       value={userName}
+      //       onChange={(e) => setUserName(e.target.value)}
+      //     />
+
+      //     <button onClick={joinRoom}>JOIN ROOM</button>
+
+      //   </div>
+      // </div>
+
+      <div className="join-container">
+        <div className="join-form">
+          <h1>Join Code Editor</h1>
+
+          <div className="room-id-container">
+            <input
+              type="text"
+              placeholder="ROOM ID"
+              value={roomId}
+              onChange={(e) => setRoomId(e.target.value)}
+            />
+          </div>
+
+          <input
+            type="text"
+            placeholder="Your Name"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+
+          <button onClick={joinRoom}>JOIN ROOM</button>
+          <button onClick={generateRoomId}>Generate Room ID</button>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="editor-container">
+      <div className="sidebar">
+        <div className="room-info">
+          <h2>Code Room: {roomId}</h2>
+          <button onClick={copyRoomId} className="copy-button">
+            Copy ID
+          </button>
+          {copySuccess && <span className="copy-successs">{copySuccess}</span>}
+        </div>
+        <h3>users in Room:</h3>
+        <ul>
+          {users.map((user, index) => (
+            <li key={index}>{user.slice(0, 8)}...</li>
+          ))}
+        </ul>
+        <p className="typing-indecator">{typing}</p>
+        <select
+          className="language-selector"
+          value={language}
+          onChange={handleLanguageChange}
+        >
+          <option value="javascript">JavaScript</option>
+          <option value="python">Python</option>
+          <option value="java">Java</option>
+          <option value="cpp">C++</option>
+        </select>
+
+        <button className="leave-button" onClick={leaveRoom}>
+          Leave ROOM
+        </button>
+      </div>
+      <div className="editor-wrapper">
+        <Editor
+          height={"100%"}
+          defaultLanguage={language}
+          language={language}
+          value={code}
+          onChange={handleCodeChange}
+          theme="vs-dark"
+          options={{
+            minimap: { enabled: false },
+            fontSize: 14,
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default App;
